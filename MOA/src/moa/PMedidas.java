ec8cc880a70dedb3f562fe72f804af75c86f428e
pedro <pedro@10.30.2.3>
2017-07-24 19:52:47 -0300

    Tudo novo

--- MOA/src/moa/PMedidas.java
+++ MOA/src/moa/PMedidas.java
@@ -5,29 +5,170 @@
  */
 package moa;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Random;
 
 /**
- *
- * @author Tatiane Paz
+ * @author Tatiane Paz e Pedro Moraes
  */
 public class PMedidas {
 
     /**
      * @param args the command line arguments
      */
+    
+    private static int numeroMedianas = 0;
+    private static int numeroVertices = 0;
+    
     public static void main(String[] args) {
-        Vetor[] vetorDados = new Vetor[101];
-        int[] medianas = new int[10];
-        lerArquivo(vetorDados);
-        populacaoInicial(vetorDados, medianas);
+        abrirArquivo();
     }
     
-     public static void lerArquivo(Vetor[] vetorDados) {
+    private static class Vertice{
+        public static int x;
+        public static int y;
+        public static int cap;
+        public static int dem;
+        public static int posicao;
+        public static int capLivre;  
+        public static boolean estado;
+        public static boolean mediana;
+        // estado = 0 -> nao visitado
+        // estado = 1 -> visitado  
+        // mediana = 0 -> nao e mediana
+        // mediana = 1 -> e mediana
+
+        public int getX() {
+            return x;
+        }
+
+        public void setX(int x) {
+            this.x = x;
+        }
+
+        public int getY() {
+            return y;
+        }
+
+        public void setY(int y) {
+            this.y = y;
+        }
+
+        public int getCap() {
+            return cap;
+        }
+
+        public void setCap(int cap) {
+            this.cap = cap;
+        }
+
+        public int getDem() {
+            return dem;
+        }
+
+        public void setDem(int dem) {
+            this.dem = dem;
+        }
+
+        public int getCapLivre() {
+            return capLivre;
+        }
+
+        public void setCapLivre(int capLivre) {
+            this.capLivre = capLivre;
+        }
+
+        public boolean isEstado() {
+            return estado;
+        }
+
+        public void setEstado(boolean estado) {
+            this.estado = estado;
+        }
+
+        public boolean isMediana() {
+            return mediana;
+        }
+
+        public void setMediana(boolean mediana) {
+            this.mediana = mediana;
+        }
+
+        public int getPosicao() {
+            return posicao;
+        }
+
+        public void setPosicao(int posicao) {
+            this.posicao = posicao;
+        }
+    }
+    
+    private static class Mediana{
+        private int posicao;
+        private Vertice mediana;
+        private ArrayList<Vertice> vertices;
+
+        public int getPosicao() {
+            return posicao;
+        }
+        public void setPosicao(int posicao) {
+            this.posicao = posicao;
+        }
+        public Vertice getMediana() {
+            return mediana;
+        }
+        public void setMediana(Vertice mediana) {
+            this.mediana = mediana;
+        }
+        public ArrayList<Vertice> getVertices() {
+            return vertices;
+        }
+        public void setVertices(ArrayList<Vertice> vertices) {
+            this.vertices = vertices;
+        }     
+    }
+    
+    private static void abrirArquivo(){
+        
+        Scanner ler = new Scanner(System.in); 
+        int contador = 0;
+        
+        System.out.printf("Digite o arquivo: ");
+        
+        int arquivo = ler.nextInt(); 
+        numeroVertices = arquivo;
+        
+        arquivo = ler.nextInt();
+        numeroMedianas = arquivo;
+        
+        Vertice vertice = new Vertice();
+        ArrayList<Vertice> vertices = new ArrayList<>();
+        
+        
+        System.out.println("Numero de vertices: " + numeroVertices);
+        System.out.println("Numero de Medianas: " + numeroMedianas);
+        
+        while(contador < numeroVertices){
+   
+            arquivo = ler.nextInt();
+            vertice.setX(arquivo);
+            arquivo = ler.nextInt();
+            vertice.setY(arquivo);
+            arquivo = ler.nextInt();
+            vertice.setCap(arquivo);
+            arquivo = ler.nextInt();
+            vertice.setDem(arquivo);
+            vertice.setPosicao(contador);
+            vertices.add(contador, vertice);
+            contador++;
+            System.out.println(vertice.getPosicao());
+        }
+        
+        //populacaoInicial(vertices);
+    }
+    
+    /*public static void lerArquivo(Vertice[] vetorDados) {
 
         Scanner ler = new Scanner(System.in);
         
@@ -72,122 +213,30 @@ public class PMedidas {
         }
         
         System.out.println();
-    }
+    }*/
+      
     
-    
-    static class Vetor{
-        public static int x = 0;
-        public static int y = 0;
-        public static int cap = 0;
-        public static int dem = 0;
-        public static int nVertices = 0;
-        public static int nMedianas = 0;
-        // estado = 0 -> nao visitado
-        // estado = 1 -> visitado
-        public static boolean estado;
-        // mediana = 0 -> nao e mediana
-        // mediana = 1 -> e mediana
-        public static boolean mediana = false;
-        public static int capLivre = 0;
-
-        public static int getCapLivre() {
-            return capLivre;
-        }
-
-        public static void setCapLivre(int capLivre) {
-            Vetor.capLivre = capLivre;
-        }
-        
-        public static int getX() {
-            return x;
-        }
-
-        public static void setX(int x) {
-            Vetor.x = x;
-        }
-
-        public static int getY() {
-            return y;
-        }
-
-        public static void setY(int y) {
-            Vetor.y = y;
-        }
-
-        public static int getCap() {
-            return cap;
-        }
-
-        public static void setCap(int cap) {
-            Vetor.cap = cap;
-        }
-
-        public static int getDem() {
-            return dem;
-        }
-
-        public static void setDem(int dem) {
-            Vetor.dem = dem;
-        }
-
-        public static int getnVertices() {
-            return nVertices;
-        }
-
-        public static void setnVertices(int nVertices) {
-            Vetor.nVertices = nVertices;
-        }
-
-        public static int getnMedianas() {
-            return nMedianas;
-        }
-
-        public static void setnMedianas(int nMedianas) {
-            Vetor.nMedianas = nMedianas;
-        }
-
-        public static boolean isEstado() {
-            return estado;
-        }
-
-        public static void setEstado(boolean estado) {
-            Vetor.estado = estado;
-        }
-
-        public static boolean isMediana() {
-            return mediana;
-        }
-
-        public static void setMediana(boolean mediana) {
-            Vetor.mediana = mediana;
-        }
-        
-    }
-    
-    public static void populacaoInicial(Vetor[] vetorDados, int[] medianas){
+    /*private static void populacaoInicial(Vertice[] vertices){
         
         //instância um objeto da classe Random usando o construtor básico
 	Random gerador = new Random();
 	int numero;
 	//imprime sequência de 10 números inteiros aleatórios entre 0 e 25
-	for (int i = 0; i < vetorDados[0].nMedianas; i++) {
+	for (int i = 0; i < numeroMedianas; i++) {
             //imprime os numeros sorteados para verificacao
             //System.out.println(gerador.nextInt(100));
             numero = gerador.nextInt(100);
-            //não pode ser 0 pois no vetor  os elementos comecao na posicao 1
-            //na posicao 0 so se encontra o num vertices e num medianas
-            //se o numero gerado aleatoriamente for 0 gera outro novamente
-            while(numero == 0)
-                numero = gerador.nextInt(100);
             //Alterado o atributo mediana para true
-            vetorDados[numero].mediana = true;
-            //Altera sua capacidadeLivre
-            vetorDados[numero].capLivre = (vetorDados[numero].cap - vetorDados[numero].dem);
-            //imprime o numero sorteado e o campo mediana para verificacao
-            System.out.printf("numero: %d -> ",numero);
-            System.out.println(vetorDados[numero].mediana);
-            //salva no vetor medianas os numeros sortidos
-            medianas[i] = numero;
+            if(!vertices[numero].mediana){
+                vertices[numero].mediana = true;
+                //Altera sua capacidadeLivre
+                vertices[numero].capLivre = (vertices[numero].cap - vertices[numero].dem);
+                //imprime o numero sorteado e o campo mediana para verificacao
+                System.out.printf("numero: %d -> ",numero);
+                System.out.println(vertices[numero].mediana);
+                //salva no vetor medianas os numeros sortidos
+                //medianas[i] = numero;
+            }
 	}
         int posicao = 1;
         double distancia;
@@ -226,13 +275,13 @@ public class PMedidas {
                     //Se não tiver capacidade passar para a proxima melhor
                     
                     //colocar o vertice na mediana de menor distancia q possui capacidade
-                    //vetorDados[medianas[j]].capLivre = (vetorDados[medianas[j]].capLivre 
-                    //                                        - vetorDados[posicao].dem); 
+                    //vetorDados[medianas[j]].capLivre = (vetorVertices[medianas[j]].capLivre 
+                    //                                        - vetorVertices[posicao].dem); 
                 }
                 posicao ++;
             }
         }//Levar em consideracao para os proximos testes q não consiga colocar 
          //todos osvertices nas medianas (penalizar conjunto de medianas escolhidas)
-    }
+    }*/
     
 }
